492fdfebab1b2ae1a18d0909c1c430516ad147d1
neal.nankivell <neal.nankivell@gmail.com>
2019-09-03 12:16:07 +0200

    Added some test cases for some edge cases * Empty list of nums * Single item list of nums * Only decreasing items * Only incresing sequence is the first and last number

--- Answer.Test/SolutionTests.cs
+++ Answer.Test/SolutionTests.cs
@@ -6,6 +6,10 @@ namespace Tests
     public class SolutionTests
     {
         [TestCase(10, 9, 2, 5, 3, 7, 101, 18, ExpectedResult = 4)]
+        [TestCase(ExpectedResult = 0)]
+        [TestCase(1, ExpectedResult = 1)]
+        [TestCase(3, 2, ExpectedResult = 1)]
+        [TestCase(10, 5, 11, ExpectedResult = 2)]
         public int LengthOfLIS(params int[] nums)
         {
             var sut = new Solution();
